68a786fb55acb8c6a02d85178dc8d550a7b2c39a
Pavel Kovalenko <pavel@kovalenko.cz>
2020-07-26 19:43:58 +0200

    Верстка сетки навигационного меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 let menu = document.querySelector(".main-nav__menu");
 let menuToggleButton = document.querySelector(".main-nav__toggle");
 
-menu.classList.add("invisible");
+// menu.classList.add("invisible");
 
-menuToggleButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  menu.classList.toggle("invisible");
-});
+// menuToggleButton.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   menu.classList.toggle("invisible");
+// });
