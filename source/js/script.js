bb6331d3d104b1124d3befe4fbb2d0c50ad2c70c
Pavel Kovalenko <pavel@kovalenko.cz>
2020-07-30 14:24:50 +0200

    Верстка меню на index.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,10 @@
-const menu = document.querySelector(".main-nav__menu");
+const menu = document.querySelector(".main-nav__list");
 const menuToggleButton = document.querySelector(".main-nav__toggle");
 
 menu.classList.add("js-invisible");
 
 menuToggleButton.addEventListener("click", function (evt) {
   evt.preventDefault();
+  console.log("Clicked");
   menu.classList.toggle("js-invisible");
 });
