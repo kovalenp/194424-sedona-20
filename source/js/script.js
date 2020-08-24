d99ab5f0cdce8b9c05f7875be61bbbe63e5ad8e8
Pavel Kovalenko <pavel@kovalenko.cz>
2020-08-24 16:10:08 +0200

    Added close button

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector(".main-nav__list");
 const menuToggleButton = document.querySelector(".main-nav__toggle");
+const menuClose = document.querySelector(".main-nav__close");
 
 menu.classList.add("js-invisible");
 
 menuToggleButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle("js-invisible");
+});
+
+menuClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.toggle("js-invisible");
 });
