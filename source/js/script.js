a22a9678bd5dc1afa6341a8c2176f41588cac59b
Pavel Kovalenko <pavel@kovalenko.cz>
2020-08-26 14:53:50 +0200

    Menu is finally done

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,22 @@
 const menu = document.querySelector(".main-nav__list");
-const menuOpen = document.querySelector(".main-nav__open");
-const menuClose = document.querySelector(".main-nav__close");
+const menuOpen = document.querySelector(".toggle__icon--open");
+const menuClose = document.querySelector(".toggle__icon--close");
 
 menu.classList.add("js-invisible");
 menuClose.classList.add("js-invisible");
 
-menuOpen.addEventListener("click", function (evt) {
-  evt.preventDefault();
+function toggleVisibility() {
   menu.classList.toggle("js-invisible");
   menuOpen.classList.toggle("js-invisible");
   menuClose.classList.toggle("js-invisible");
+}
+
+menuOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  toggleVisibility();
 });
 
 menuClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menu.classList.toggle("js-invisible");
-  menuOpen.classList.toggle("js-invisible");
-  menuClose.classList.toggle("js-invisible");
+  toggleVisibility();
 });
