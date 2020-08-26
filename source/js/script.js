81f329d74156aec512ebc35e258ac2de73eadddc
Pavel Kovalenko <pavel@kovalenko.cz>
2020-08-26 14:21:44 +0200

    Update menu close/open logic

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,20 @@
 const menu = document.querySelector(".main-nav__list");
-const menuToggleButton = document.querySelector(".main-nav__toggle");
+const menuOpen = document.querySelector(".main-nav__open");
 const menuClose = document.querySelector(".main-nav__close");
 
 menu.classList.add("js-invisible");
+menuClose.classList.add("js-invisible");
 
-menuToggleButton.addEventListener("click", function (evt) {
+menuOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.toggle("js-invisible");
+  menuOpen.classList.toggle("js-invisible");
+  menuClose.classList.toggle("js-invisible");
 });
 
 menuClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.toggle("js-invisible");
+  menuOpen.classList.toggle("js-invisible");
+  menuClose.classList.toggle("js-invisible");
 });
